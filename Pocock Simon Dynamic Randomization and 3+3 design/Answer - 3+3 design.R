ce7b55bde7eb5db86b58fb71180b3c9797d08667
Asif Rahman <ar00062@mix.wvu.edu>
2023-12-11 14:34:04 -0500

    Update Answer - 3+3 design.R

--- Pocock Simon Dynamic Randomization and 3+3 design/Answer - 3+3 design.R
+++ Pocock Simon Dynamic Randomization and 3+3 design/Answer - 3+3 design.R
@@ -14,7 +14,7 @@ N <- 10000
 
 pT<- 0.10
 
-# Put 1 if dose-escalation is happening. Put 0 otherwise.
+# Put 1 if dose-escalation is happening. 
 
 # ( Here, Otherwise Means = MTD or no dose-escalation)
 
